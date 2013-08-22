3c6937e3669c1339a108247c479e0493c60eeaf6
jazzqueenjessie <jazzqueenjessie@gmail.com>
2013-08-22 16:10:13 -0700

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jordanandjessie2014::Application.config.secret_token = 'd3e03223b7d6fb3e25cdc362a5fe603dd5551457c4eafbb33e230e4b0482a8af32e9b4a92f33b9f54bfe1115ac43d38b8ec1f0a13ecdff5da991693ccc349411'
